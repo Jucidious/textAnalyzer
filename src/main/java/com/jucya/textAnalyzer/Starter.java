774d396ea2c95a252e7f81bb46a96e2580927370
69VinnichenkoSG <69vinnichenkosg@region.cbr.ru>
2023-09-17 19:10:48 +0700

    выпилил gitProperties для конкурса, поправил чекстайл

--- src/main/java/com/jucya/textAnalyzer/Starter.java
+++ src/main/java/com/jucya/textAnalyzer/Starter.java
@@ -1,11 +1,8 @@
 package com.jucya.textAnalyzer;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.Banner;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.info.GitProperties;
 
 /**
  * Запускает {@code TextAnalyzer} приложение.
@@ -14,13 +11,6 @@ import org.springframework.boot.info.GitProperties;
  */
 @SpringBootApplication
 public class Starter {
-
-    private static final Logger LOGGER = LoggerFactory.getLogger(Starter.class);
-    private static final String BUILD_VERSION_KEY = "build.version";
-
-    public Starter(GitProperties gitProperties) {
-        printBuildInfo(gitProperties);
-    }
 
     /**
      * Входная точка в сервисы анализа текста.
@@ -32,15 +22,5 @@ public class Starter {
             .sources(Starter.class)
             .bannerMode(Banner.Mode.OFF)
             .run(args);
-    }
-
-    private void printBuildInfo(GitProperties gitProperties) {
-        LOGGER.info(
-            "Running TextAnalyzer App version {} commit {} build on {} at {}",
-            gitProperties.get(BUILD_VERSION_KEY),
-            gitProperties.getShortCommitId(),
-            gitProperties.getBranch(),
-            gitProperties.getCommitTime()
-        );
     }
 }
